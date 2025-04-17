de3cf1d6ee49be044af99c85b1370beadedd9c07
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-04-17 13:06:00 -0500

    Build: Integrate Tailwind CSS into the project

---
+++ frontend/admin/includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/public/images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="/public/css/output.css">
+    <title>PANEL ADMINISTRATIVO ENERSOLAR</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
